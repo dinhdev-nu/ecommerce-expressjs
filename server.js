a8301b95546058538a66f01617f7b2321dd94bd2
dinhdev-nu <truongbadinh13579@gmail.com>
2025-01-23 23:00:23 +0700

    push for some changes

--- server.js
+++ server.js
@@ -8,4 +8,8 @@ const server = app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
 
-process.on("SIGINT", () => console.log("Server is shutting down..."));
+process.on("SIGINT", () => {
+    server.close( () => {
+        console.log("Server closed");
+    })
+});
