4ed9a1e1611fddf5ee305879930743626595bb01
dinhdev-nu <truongbadinh13579@gmail.com>
2025-07-29 13:04:27 +0700

    v1: last commit

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
-const app = require('./src/app');
+const app = require("./src/app");
 
-require('dotenv').config();
+require("dotenv").config();
 
 const PORT = process.env.SERVER_PORT || 3000;
 
 const server = app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
 
 process.on("SIGINT", () => {
-    server.close( () => {
-        console.log("Server closed");
-    })
+  server.close(() => {
+    console.log("Server closed");
+  });
 });
